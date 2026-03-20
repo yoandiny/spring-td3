ff64de4b59796351bacee7ee13f93f3eacbc1a3d
DINY Fondàna Yoan <yoandiny14@gmail.com>
2026-03-20 19:16:06 +0300

    initial commit

---
+++ src/main/java/mg/yoan/td3/Td3Application.java
@@ -0,0 +1,13 @@
+package mg.yoan.td3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td3Application.class, args);
+	}
+
+}
---
+++ src/test/java/mg/yoan/td3/Td3ApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.yoan.td3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
